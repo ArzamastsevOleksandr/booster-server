6ce7558c207fc0fbe4af75bd8ff8fede3c01826e
oleksandr.arzamastsev <sarzamastsev5@gmail.com>
2022-12-29 18:41:37 +0200

    Initial commit

---
+++ src/main/java/com/booster/server/BoosterServerApplication.java
@@ -0,0 +1,13 @@
+package com.booster.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoosterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoosterServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/booster/server/BoosterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booster.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoosterServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
