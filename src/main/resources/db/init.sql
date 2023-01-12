666ae045078ada0d05e698ca5b42fd03a4bb911f
oleksandr.arzamastsev <sarzamastsev5@gmail.com>
2023-01-12 23:57:08 +0200

    refactor: PR fixes

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -1,20 +1,26 @@
+-- todo: benchmark the sequence cache value
+CREATE SEQUENCE note_id_sequence
+    CACHE 50;
+
 CREATE TABLE note
 (
-    id      BIGINT PRIMARY KEY,
-    content TEXT
+    id           BIGINT PRIMARY KEY DEFAULT nextval('note_id_sequence'),
+    content      TEXT,
+    last_seen_at TIMESTAMP
 );
 
-CREATE SEQUENCE note_id_sequence
-    CACHE 50
-    OWNED BY note.id;
+ALTER SEQUENCE note_id_sequence OWNED BY note.id;
+
+
+CREATE SEQUENCE vocabulary_entry_id_sequence
+    CACHE 50;
 
 CREATE TABLE vocabulary_entry
 (
-    id          BIGINT PRIMARY KEY,
-    name        text NOT NULL,
-    description TEXT
+    id           BIGINT PRIMARY KEY DEFAULT nextval('vocabulary_entry_id_sequence'),
+    name         TEXT NOT NULL,
+    description  TEXT,
+    last_seen_at TIMESTAMP
 );
 
-CREATE SEQUENCE vocabulary_entry_id_sequence
-    CACHE 50
-    OWNED BY vocabulary_entry.id;
+ALTER SEQUENCE vocabulary_entry_id_sequence OWNED BY vocabulary_entry.id;
